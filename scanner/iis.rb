a6bbcca4bb85bd42e8ae744d276b8a42e23c8bfe
Adam Caudill <adam@adamcaudill.com>
2013-02-28 12:08:24 -0500

    PHP != IIS

--- scanner/iis.rb
+++ scanner/iis.rb
@@ -1,5 +1,5 @@
 def iis_check_banner(banner)
-  #don't bother if this doesn't include PHP
+  #don't bother if this doesn't include IIS
   return if !banner.include? 'Microsoft-IIS/'
 
   puts_warn "IIS Version: #{banner}"
