7e7853f986756d7da88ce0373c2fbe5b4789586f
Adam Caudill <adam@adamcaudill.com>
2013-03-25 16:23:43 -0400

    Adding a couple notes

--- lib/commands/scan.rb
+++ lib/commands/scan.rb
@@ -4,12 +4,14 @@ module Yawast
       def self.process(args, options)
         raise ArgumentError.new('You must specify a URL.') if args.empty?
 
+        #this might be a bad assumption
         url = args[0]
 
         uri = URI.parse(url)
         uri.path = '/' if uri.path == ''
 
         #see if we can resolve the host
+        # we don't really need it, it just serves as validation
         begin
           dns = Resolv::DNS.new()
           dns.getaddress(uri.host)
