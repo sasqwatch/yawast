c04bb961eaa2caed874f642069a17fd56a686949
Adam Caudill <adam@adamcaudill.com>
2013-04-23 15:43:34 -0400

    Make check_path more generic

--- lib/scanner/source_control.rb
+++ lib/scanner/source_control.rb
@@ -2,20 +2,27 @@ module Yawast
   module Scanner
     class SourceControl
       def self.check_all(uri)
-        check_path(uri, '.git')
-        check_path(uri, '.hg')
-        check_path(uri, '.svn')
-        check_path(uri, '.bzr')
+        check_path(uri, '.git', true)
+        check_path(uri, '.hg', true)
+        check_path(uri, '.svn', true)
+        check_path(uri, '.bzr', true)
       end
 
-      def self.check_path(uri, path)
+      def self.check_path(uri, path, vuln)
         #note: this only checks directly at the root, I'm not sure if this is what we want
         # should probably be relative to what's passed in, instead of overriding the path.
         uri.path = "/#{path}/"
         code = Yawast::Shared::Http.get_status_code(uri)
 
         if code == 200
-          Yawast::Utilities.puts_vuln "'#{path}' Source Control Directory found: #{uri}"
+          msg = "'#{path}' Source Control Directory found: #{uri}"
+
+          if vuln
+            Yawast::Utilities.puts_vuln msg
+          else
+            Yawast::Utilities.puts_warn msg
+          end
+
           puts ''
         end
       end
