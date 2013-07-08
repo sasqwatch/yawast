3da543237705be6f035b8e0c11feca5f4504310e
Adam Caudill <adam@adamcaudill.com>
2013-07-08 01:17:07 -0400

    Add HTTP unit tests

---
+++ test/test_shared_http.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require './lib/yawast'
+require './test/base'
+
+class TestSharedHttp < MiniTest::Unit::TestCase
+  include TestBase
+
+  def setup
+    @uri = URI::Parser.new.parse 'http://www.apple.com/library/test/success.html'
+  end
+
+  def test_get_apple_success
+    body = Yawast::Shared::Http.get @uri
+
+    assert body.include?('Success'), 'Failed to receive "Success" message from Apple.com'
+  end
+
+  def test_status_apple_success
+    status = Yawast::Shared::Http.get_status_code @uri
+
+    assert_equal status, '200'
+  end
+
+  def test_status_apple_failure
+    uri = @uri
+    uri.path += '.404'
+    status = Yawast::Shared::Http.get_status_code uri
+
+    assert_equal status, '404'
+  end
+
+  def test_head_apple_success
+    head = Yawast::Shared::Http.head @uri
+    head.each do |k, v|
+      if k.downcase == 'server'
+        assert_equal v, 'Apache'
+      end
+    end
+  end
+end
