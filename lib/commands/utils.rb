30e0ccccde2451d10a91e0ed1f52d5c7b1411fe1
Adam Caudill <adam@adamcaudill.com>
2016-06-07 18:05:22 -0400

    Make sure the path always ends in a slash

--- lib/commands/utils.rb
+++ lib/commands/utils.rb
@@ -10,8 +10,15 @@ module Yawast
         #this might be buggy - actually, I know it is...
         url = 'http://' + url unless url.include?('http://') || url.include?('https://')
 
+        #make sure the path is at least a slash
         uri = URI.parse(url)
         uri.path = '/' if uri.path == ''
+
+        #this is buggy, but we don't handle files anyhow...
+        #if the path doesn't end in a slash, add one.
+        if uri.path[-1, 1] != '/'
+          uri.path.concat '/'
+        end
 
         #see if we can resolve the host
         # we don't really need it, it just serves as validation
