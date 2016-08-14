8915f6f92d719007d97c4b51d5cece21d914e443
Adam Caudill <adam@adamcaudill.com>
2016-08-14 02:18:28 -0400

    Start building out the display code
    So far, this is working out well.

--- lib/scanner/ssl_labs.rb
+++ lib/scanner/ssl_labs.rb
@@ -4,7 +4,7 @@ module Yawast
   module Scanner
     class SslLabs
       def self.info(uri)
-        puts 'Beginning SSL Labs scan (this could take a minute)'
+        puts 'Beginning SSL Labs scan (this could take a minute or two)'
 
         api = Ssllabs::Api.new
 
@@ -31,8 +31,35 @@ module Yawast
           puts
 
           host.endpoints.each do |ep|
-            Yawast::Utilities.puts_info "IP: #{ep.ip_address}"
-            Yawast::Utilities.puts_info "\tGrade#{ep.grade}"
+            Yawast::Utilities.puts_info "IP: #{ep.ip_address} - Grade: #{ep.grade}"
+            puts
+
+            # get the ChainCert info for the server cert - needed for extra details
+            cert = nil
+            ep.details.chain.certs.each do |c|
+              if c.subject == ep.details.cert.subject
+                cert = c
+              end
+            end
+
+            puts "\tCertificate Information:"
+            unless ep.details.cert.valid?
+              Yawast::Utilities.puts_vuln "\t\tCertificate Not Valid"
+            end
+
+            Yawast::Utilities.puts_info "\t\tSubject: #{ep.details.cert.subject}"
+            Yawast::Utilities.puts_info "\t\tCommon Names: #{ep.details.cert.common_names}"
+
+            Yawast::Utilities.puts_info "\t\tAlternative names:"
+            ep.details.cert.alt_names.each do |name|
+              Yawast::Utilities.puts_info "\t\t\t#{name}"
+            end
+
+            Yawast::Utilities.puts_info "\t\tNot Before: #{ep.details.cert.not_before}"
+            Yawast::Utilities.puts_info "\t\tNot After: #{ep.details.cert.not_after}"
+
+            Yawast::Utilities.puts_info "\t\tKey: #{cert.key_alg} #{cert.key_size}"
+
             puts
           end
 
