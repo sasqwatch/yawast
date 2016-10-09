a364eb0121a69073145fe31995396969e67f3ea6
Adam Caudill <adam@adamcaudill.com>
2016-10-09 15:17:52 -0400

    Revert "Update coverage setup, again."
    This reverts commit 308dd2a419dc8c9c7c4efdb6d50fcb3104ef11b0.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,6 @@ require 'minitest/reporters'
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.start do
-  add_filter do |source_file|
-    source_file.filename =~ /test/
-  end
-end
-
+SimpleCov.start
 MiniTest::Reporters.use!
 Coveralls.wear!
