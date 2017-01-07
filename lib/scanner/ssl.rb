83ff373e11baa23dad9fea6a3c3e7dfb44c40add
Adam Caudill <adam@adamcaudill.com>
2017-01-07 14:53:27 -0500

    Fix formatting

--- lib/scanner/ssl.rb
+++ lib/scanner/ssl.rb
@@ -216,52 +216,52 @@ module Yawast
         puts ''
       end
 
-        def self.get_tdes_session_msg_count(uri)
-          # this method will send a number of HEAD requests to see
-          #  if the connection is eventually killed.
-          puts 'TLS Session Request Limit: Checking number of requests accepted using 3DES suites...'
+      def self.get_tdes_session_msg_count(uri)
+        # this method will send a number of HEAD requests to see
+        #  if the connection is eventually killed.
+        puts 'TLS Session Request Limit: Checking number of requests accepted using 3DES suites...'
 
-          count = 0
-          begin
-            req = Yawast::Shared::Http.get_http(uri)
-            req.use_ssl = uri.scheme == 'https'
-            req.keep_alive_timeout = 600
-            headers = Yawast::Shared::Http.get_headers
+        count = 0
+        begin
+          req = Yawast::Shared::Http.get_http(uri)
+          req.use_ssl = uri.scheme == 'https'
+          req.keep_alive_timeout = 600
+          headers = Yawast::Shared::Http.get_headers
 
-            #force 3DES - this is to ensure that 3DES specific limits are caught
-            req.ciphers = ['3DES']
+          #force 3DES - this is to ensure that 3DES specific limits are caught
+          req.ciphers = ['3DES']
 
-            req.start do |http|
-              10000.times do |i|
-                http.head(uri.path, headers)
+          req.start do |http|
+            10000.times do |i|
+              http.head(uri.path, headers)
 
-                # hack to detect transparent disconnects
-                if http.instance_variable_get(:@ssl_context).session_cache_stats[:cache_hits] != 0
-                  raise 'TLS Reconnected'
-                end
+              # hack to detect transparent disconnects
+              if http.instance_variable_get(:@ssl_context).session_cache_stats[:cache_hits] != 0
+                raise 'TLS Reconnected'
+              end
 
-                count += 1
+              count += 1
 
-                if i % 20 == 0
-                  print '.'
-                end
+              if i % 20 == 0
+                print '.'
               end
             end
-          rescue => e
-            puts
+          end
+        rescue => e
+          puts
 
-            if e.message.include? 'alert handshake failure'
-              Yawast::Utilities.puts_info 'TLS Session Request Limit: Server does not support 3DES cipher suites'
-            else
-              Yawast::Utilities.puts_info "TLS Session Request Limit: Connection terminated after #{count} requests (#{e.message})"
-            end
-
-            return
+          if e.message.include? 'alert handshake failure'
+            Yawast::Utilities.puts_info 'TLS Session Request Limit: Server does not support 3DES cipher suites'
+          else
+            Yawast::Utilities.puts_info "TLS Session Request Limit: Connection terminated after #{count} requests (#{e.message})"
           end
 
-          puts
-          Yawast::Utilities.puts_vuln 'TLS Session Request Limit: Connection not terminated after 10,000 requests; possibly vulnerable to SWEET32'
+          return
         end
+
+        puts
+        Yawast::Utilities.puts_vuln 'TLS Session Request Limit: Connection not terminated after 10,000 requests; possibly vulnerable to SWEET32'
+      end
 
       #private methods
       class << self
