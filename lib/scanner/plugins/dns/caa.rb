c036b4aa7f0e51be8147cbe9ff5b54b10cb1e332
Adam Caudill <adam@adamcaudill.com>
2017-09-26 14:11:50 -0400

    Handle CNAMEs

--- lib/scanner/plugins/dns/caa.rb
+++ lib/scanner/plugins/dns/caa.rb
@@ -22,7 +22,9 @@ module Yawast
                   ans.answer.each do |rec|
                     # check for CNAME first
                     if rec.type == 'CNAME'
-                      Yawast::Utilities.puts_error "\t\tCAA (#{domain}): CNAME Found: Not Currently Supported"
+                      Yawast::Utilities.puts_info "\t\tCAA (#{domain}): CNAME Found: -> #{rec.rdata}"
+
+                      follow_cname res,rec
                     else
                       # check for RDATA
                       if rec.rdata != nil
@@ -42,6 +44,36 @@ module Yawast
 
               # strip the leading element off the domain
               domain = domain.partition('.').last
+            end
+          end
+
+          def self.follow_cname(res, rec)
+            # we have a CNAME, so we should check the target, and see if it has anything
+            #  then, we'll continue searching the chain.
+            cname = rec.rdata
+            while cname != ''
+              cname_ans = res.query(cname, 'CAA')
+
+              if cname_ans.answer.count > 0
+                ans.answer.each do |record|
+                  if record.type == 'CNAME'
+                    # another CNAME
+                    Yawast::Utilities.puts_info "\t\tCAA (#{domain}): CNAME Found: -> #{rec.rdata}"
+                    cname = rec.rdata
+                  else
+                    if record.rdata != nil
+                      Yawast::Utilities.puts_info "\t\tCAA (#{domain}): #{record.rdata}"
+                    else
+                      Yawast::Utilities.puts_error "\t\tCAA (#{domain}): Invalid Response: #{record.answer}"
+                    end
+
+                    cname = ''
+                  end
+                end
+              else
+                Yawast::Utilities.puts_info "\t\tCAA (#{cname}): No Records Found"
+                cname = ''
+              end
             end
           end
         end
