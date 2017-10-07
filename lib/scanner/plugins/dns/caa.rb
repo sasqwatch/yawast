186137b236b30601e1f9b0d70ff99f5fdbdc1a5f
Adam Caudill <adam@adamcaudill.com>
2017-10-07 15:45:18 -0400

    Rewrite CAA checking code
    This is a lot cleaner, and fixes #114

--- lib/scanner/plugins/dns/caa.rb
+++ lib/scanner/plugins/dns/caa.rb
@@ -8,35 +8,37 @@ module Yawast
         class CAA
           def self.caa_info(uri)
             # force DNS resolver to something that works
-            res = Resolver.new({:nameserver => ['8.8.8.8']})
+            # this is done to ensure that ISP resolvers don't get in the way
+            # at some point, should probably do something else, but works for now
+            @res = Resolver.new({:nameserver => ['8.8.8.8']})
+
+            # setup a list of domains already checked, so we can skip them
+            @checked = Array.new
 
             domain = uri.host.to_s
-            
+
+            chase_domain domain
+          end
+
+          def self.chase_domain(domain)
             while domain != '' do
               begin
-                ans = res.query(domain, 'CAA')
-
-                # check if we have any response
-                if ans.answer.count > 0
-                  ans.answer.each do |rec|
-                    # check for CNAME first
-                    if rec.type == 'CNAME'
-                      Yawast::Utilities.puts_info "\t\tCAA (#{domain}): CNAME Found: -> #{rec.rdata}"
-
-                      follow_cname res,rec
-                    else
-                      # check for RDATA
-                      if rec.rdata != nil
-                        Yawast::Utilities.puts_info "\t\tCAA (#{domain}): #{rec.rdata}"
-                      else
-                        Yawast::Utilities.puts_error "\t\tCAA (#{domain}): Invalid Response: #{ans.answer}"
-                      end
-                    end
-                  end
-                else
-                  Yawast::Utilities.puts_info "\t\tCAA (#{domain}): No Records Found"
+                # check to see if we've already ran into this one
+                if @checked.include? domain
+                  return
                 end
+                @checked.push domain
 
+                # first, see if this is a CNAME. we do this explicitly because
+                # some resolvers flatten in an odd way that prevents just checking
+                # for the CAA record directly
+                cname = get_cname_record(domain)
+                if cname != nil
+                  Yawast::Utilities.puts_info "\t\tCAA (#{domain}): CNAME Found: -> #{cname}"
+                  chase_domain cname.to_s
+                else
+                  print_caa_record domain
+                end
               rescue => e
                 Yawast::Utilities.puts_error "\t\tCAA (#{domain}): #{e.message}"
               end
@@ -46,33 +48,31 @@ module Yawast
             end
           end
 
-          def self.follow_cname(res, rec)
-            # we have a CNAME, so we should check the target, and see if it has anything
-            #  then, we'll continue searching the chain.
-            cname = rec.rdata
-            while cname != ''
-              cname_ans = res.query(cname, 'CAA')
+          def self.get_cname_record(domain)
+            ans = @res.query(domain, 'CNAME')
 
-              if cname_ans.answer.count > 0
-                ans.answer.each do |record|
-                  if record.type == 'CNAME'
-                    # another CNAME
-                    Yawast::Utilities.puts_info "\t\tCAA (#{domain}): CNAME Found: -> #{rec.rdata}"
-                    cname = rec.rdata
-                  else
-                    if record.rdata != nil
-                      Yawast::Utilities.puts_info "\t\tCAA (#{domain}): #{record.rdata}"
-                    else
-                      Yawast::Utilities.puts_error "\t\tCAA (#{domain}): Invalid Response: #{record.answer}"
-                    end
+            if ans.answer[0] != nil
+              return ans.answer[0].rdata
+            else
+              return nil
+            end
+          end
 
-                    cname = ''
-                  end
+          def self.print_caa_record(domain)
+            ans = @res.query(domain, 'CAA')
+
+            if ans.answer.count > 0
+              ans.answer.each do |rec|
+                # check for RDATA
+                if rec.rdata != nil
+                  Yawast::Utilities.puts_info "\t\tCAA (#{domain}): #{rec.rdata}"
+                else
+                  Yawast::Utilities.puts_error "\t\tCAA (#{domain}): Invalid Response: #{ans.answer}"
                 end
-              else
-                Yawast::Utilities.puts_info "\t\tCAA (#{cname}): No Records Found"
-                cname = ''
               end
+            else
+              # no answer, so no records
+              Yawast::Utilities.puts_info "\t\tCAA (#{domain}): No Records Found"
             end
           end
         end
