c5a0349163e6d30591eb3ee3d0c86f84e781092d
Adam Caudill <adam@adamcaudill.com>
2019-06-18 11:30:05 -0400

    Scan the response for other issues

--- yawast/scanner/plugins/http/servers/rails.py
+++ yawast/scanner/plugins/http/servers/rails.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from yawast.reporting.enums import Vulnerabilities
+from yawast.scanner.plugins.http import response_scanner
 from yawast.scanner.plugins.result import Result
-from yawast.shared import output, network
-from typing import List, Dict, Tuple
+from yawast.shared import network
 
 
 def check_cve_2019_5418(url: str) -> List[Result]:
@@ -16,6 +18,8 @@ def check_cve_2019_5418(url: str) -> List[Result]:
     )
     body = res.text
     req = network.http_build_raw_request(res.request)
+
+    results += response_scanner.check_response(url, res)
 
     if "root:" in body:
         results.append(
