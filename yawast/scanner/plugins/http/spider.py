b4a54ea668b3c04c0b50a9f891225ef6a5db0790
Adam Caudill <adam@adamcaudill.com>
2019-06-19 21:00:40 -0400

    Less recursion in the spider.

--- yawast/scanner/plugins/http/spider.py
+++ yawast/scanner/plugins/http/spider.py
@@ -15,7 +15,7 @@ _insecure = []
 
 def spider(url) -> Tuple[List[str], List[Result]]:
     global _links, _results, _insecure
-    _get_links(url, url)
+    _get_links(url, [url])
 
     # copy data and reset
     links = _links[:]
@@ -27,80 +27,85 @@ def spider(url) -> Tuple[List[str], List[Result]]:
     return links, results
 
 
-def _get_links(base_url: str, url: str):
+def _get_links(base_url: str, urls: List[str]):
     global _links, _results, _insecure
 
-    res = network.http_get(url, False)
+    for url in urls:
+        queue = []
 
-    if "Content-Type" in res.headers and "text/html" in res.headers["Content-Type"]:
-        soup = BeautifulSoup(res.text, "html.parser")
-    else:
-        _results += response_scanner.check_response(url, res)
+        res = network.http_get(url, False)
 
-        return
+        if "Content-Type" in res.headers and "text/html" in res.headers["Content-Type"]:
+            soup = BeautifulSoup(res.text, "html.parser")
+        else:
+            _results += response_scanner.check_response(url, res)
 
-    # check the response for issues
-    _results += response_scanner.check_response(url, res, soup)
+            return
 
-    for link in soup.find_all("a"):
-        href = link.get("href")
+        # check the response for issues
+        _results += response_scanner.check_response(url, res, soup)
 
-        if href is not None:
-            # check to see if this link is in scope
-            if base_url in href and href not in _links:
-                if "." in href.split("/")[-1]:
-                    file_ext = href.split("/")[-1].split(".")[-1]
-                else:
-                    file_ext = None
+        for link in soup.find_all("a"):
+            href = link.get("href")
 
-                _links.append(href)
+            if href is not None:
+                # check to see if this link is in scope
+                if base_url in href and href not in _links:
+                    if "." in href.split("/")[-1]:
+                        file_ext = href.split("/")[-1].split(".")[-1]
+                    else:
+                        file_ext = None
 
-                # filter out some of the obvious binary files
-                if file_ext is None or file_ext not in [
-                    "gzip",
-                    "jpg",
-                    "jpeg",
-                    "gif",
-                    "woff",
-                    "zip",
-                    "exe",
-                    "gz",
-                    "pdf",
-                ]:
-                    _get_links(base_url, href)
-                else:
-                    output.debug(
-                        f'Skipping URL "{href}" due to file extension "{file_ext}"'
-                    )
-            else:
-                # TODO: Check PSL, if outside of scope, it's an issue
+                    _links.append(href)
 
-                if (
-                    "https://" in base_url
-                    and "http://" in href
-                    and href not in _insecure
-                ):
-                    # link from secure to insecure
-                    _insecure.append(href)
-
-                    _results.append(
-                        Result(
-                            f"Insecure Link: {url} links to {href}",
-                            Vulnerabilities.HTTP_INSECURE_LINK,
-                            url,
-                            [href, res.text],
+                    # filter out some of the obvious binary files
+                    if file_ext is None or file_ext not in [
+                        "gzip",
+                        "jpg",
+                        "jpeg",
+                        "gif",
+                        "woff",
+                        "zip",
+                        "exe",
+                        "gz",
+                        "pdf",
+                    ]:
+                        queue.append(href)
+                    else:
+                        output.debug(
+                            f'Skipping URL "{href}" due to file extension "{file_ext}"'
                         )
-                    )
-                pass
+                else:
+                    # TODO: Check PSL, if outside of scope, it's an issue
 
-    # handle redirects
-    if "Location" in res.headers:
-        redirect = res.headers["Location"]
+                    if (
+                        "https://" in base_url
+                        and "http://" in href
+                        and href not in _insecure
+                    ):
+                        # link from secure to insecure
+                        _insecure.append(href)
 
-        # check for relative link
-        if str(redirect).startswith("/"):
-            redirect = urljoin(base_url, redirect)
+                        _results.append(
+                            Result(
+                                f"Insecure Link: {url} links to {href}",
+                                Vulnerabilities.HTTP_INSECURE_LINK,
+                                url,
+                                [href, res.text],
+                            )
+                        )
+                    pass
 
-        # make sure that we aren't redirected out of scope
-        if base_url in redirect:
-            _get_links(base_url, redirect)
+        # handle redirects
+        if "Location" in res.headers:
+            redirect = res.headers["Location"]
+
+            # check for relative link
+            if str(redirect).startswith("/"):
+                redirect = urljoin(base_url, redirect)
+
+            # make sure that we aren't redirected out of scope
+            if base_url in redirect:
+                queue.append(redirect)
+
+        _get_links(base_url, queue)
